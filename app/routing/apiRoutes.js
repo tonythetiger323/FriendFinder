633a1bc2983091f3768535fd1e8e54732f0f0afe
Tony Greeley <tony.greeley@me.com>
2018-12-04 18:26:12 -0600

    Completed layout of apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,3 +2,14 @@
 const friends = require("../data/friends.js");
 
 //Routing
+module.exports = (app) => {
+    //API get requests
+    app.get("/api/friends", (req, res) => {
+        res.json(friends);
+    });
+
+    app.post("/api/friends", (req, res) => {
+
+    });
+
+};
