0f81ba6149013d6a37fe80aa5305e95874305377
Tony Greeley <tony.greeley@me.com>
2018-12-06 14:51:11 -0600

    appears to be working, will need to polish

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+const friendsArray = [
+    {
+        name: "Tony",
+        photo: "https://media-assets-05.thedrum.com/cache/images/thedrum-prod/public-drum_basic_article-101205-main_images-tony-the-tiget-scarf_0--default--216.jpg",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    }
+]
+
+module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,7 +10,41 @@ module.exports = (app) => {
 
     //API post requests
     app.post("/api/friends", (req, res) => {
+        const postFriend = req.body;
+        let difference = 40;
+        let matchedName = "";
+        let matchedPhoto = "";
+        friends.forEach((friend) => {
+            console.log('Comparing to:');
+            console.log(friend.name);
 
+            let matchScoreArray = [];
+            for (let i = 0; i < friend.scores.length; i++) {
+                matchScoreArray.push(Math.abs((postFriend.scores[i]) - (friend.scores[i])));
+            }
+
+            console.log('MatchScoreArray: ' + matchScoreArray);
+
+            const reducer = (accumulator, currentValue) => accumulator + currentValue;
+            let totalDifference = matchScoreArray.reduce(reducer);
+
+            console.log('The Total Difference Is: ' + totalDifference);
+
+            if (totalDifference < difference) {
+                difference = totalDifference;
+                matchedName = friend.name;
+                matchedPhoto = friend.photo;
+            }
+            console.log('Matched Friend Name:' + matchedName);
+        });
+        res.json({
+            name: matchedName,
+            photo: matchedPhoto
+        });
+
+        friends.push(postFriend);
+        console.log('Contents of Friends Array: ')
+        console.log(friends);
     });
 
 };
